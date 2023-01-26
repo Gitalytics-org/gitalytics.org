4e69b624dff2b4e47aebf5b14bae497454c9ed56
roriwa <robinrwalter2002@gmail.com>
2023-01-26 12:03:42 +0100

    included react-query for api calls

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,4 +1,26 @@
-export default function App() {
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            cacheTime: 300000, // 5min
+            staleTime: 60000, // 1min
+            refetchOnMount: false,
+            refetchOnReconnect: true,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
+export default function AppWrapper() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
+    );
+}
+
+export function App() {
     return (
         <div>
             <h1 className='text-4xl text-center'>Hello World</h1>
