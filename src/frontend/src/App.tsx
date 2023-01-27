f4ebb1be1ee655a716d6b0b4485907d140fc4d27
roriwa <robinrwalter2002@gmail.com>
2023-01-27 12:02:43 +0100

    configured prettier

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <div>
-            <h1 className='text-4xl text-center'>Hello World</h1>
+            <h1 className="text-4xl text-center">Hello World</h1>
         </div>
     );
 }
