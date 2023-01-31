a9d9ddd7776e58ca0b7586d97193549d6d8dafcf
Julius Jänchen <julius.jaenchen@check24.de>
2023-01-31 11:22:06 +0100

    small improvements

--- src/backend/api/routes/get_workspaces.route.py
+++ src/backend/api/routes/get_workspaces.route.py
@@ -12,22 +12,30 @@ from api.database.db import createLocalSession
 
 router = fastapi.APIRouter()
 
-class ResponseModel(pydantic.BaseModel):
-    workspaces: typing.List[Workspace]
+
+class WorkspaceViewModel(pydantic.BaseModel):
+    id: int
+    name: str
 
     class Config:
-        orm_mode = True
-        arbitrary_types_allowed = True
+        orm_mode: True
+
+class ViewModel(pydantic.BaseModel):
+    workspaces: typing.List[WorkspaceViewModel]
+
+    class Config:
+        orm_mode: True
 
 
-@router.get("/get-workspaces", response_model=ResponseModel)
+
+@router.get("/get-workspaces", response_model=ViewModel)
 async def create_workspace():
     r"""
     test the creation of a workspace
     """
     with createLocalSession() as session:
-        workspaces = session.query(Workspace).select()
+        workspaces = session.query(Workspace).all()
     
-        
+    print(workspaces)
 
     return {"workspaces": workspaces}
