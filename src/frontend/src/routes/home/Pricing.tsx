ff0fd76672f21f5a666c8605f209fcecfdd5be59
roriwa <robinrwalter2002@gmail.com>
2023-02-10 16:13:58 +0100

    fixed colors (pricing)

--- src/frontend/src/routes/home/Pricing.tsx
+++ src/frontend/src/routes/home/Pricing.tsx
@@ -12,13 +12,13 @@ export default function Pricing() {
 
 
 function FreeTier() {
-    return <div className="border border-slate-300 min-w-[400px] text-center rounded-lg select-none">
-        <h2 className="text-3xl text-gitalytics bg-slate-300 p-1">Free Tier</h2>
-        <div className="bg-slate-200 p-1">All your Repositories</div>
-        <div className="bg-slate-100 p-1">Insights over the full history</div>
-        <div className="bg-slate-200 p-1">Many Graphs</div>
-        <div className="bg-slate-100 p-1">
-            <Link to="/login" className="w-3/4 mx-auto bg-gitalytics bg-opacity-40 hover:bg-opacity-70 rounded-lg block py-1">
+    return <div className="border border-secondary min-w-[400px] text-center rounded-lg select-none">
+        <h2 className="text-3xl text-gitalytics p-1 bg-secondary bg-opacity-20 text-accent">Free Tier</h2>
+        <div className="bg-secondary bg-opacity-5 p-1">All your Repositories</div>
+        <div className="bg-secondary bg-opacity-10 p-1">Insights over the full history</div>
+        <div className="bg-secondary bg-opacity-5 p-1">Many Graphs</div>
+        <div className="bg-secondary bg-opacity-10 p-1">
+            <Link to="/login" className="w-3/4 mx-auto bg-accent bg-opacity-40 hover:bg-opacity-70 rounded-lg block py-1">
                 Do it now
             </Link>
         </div>
