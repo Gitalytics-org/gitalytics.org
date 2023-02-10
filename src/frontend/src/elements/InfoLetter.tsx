46e7955538c20e4d3070e8d6eaf40ee59a8866f4
roriwa <robinrwalter2002@gmail.com>
2023-02-10 17:01:50 +0100

    fixed infoletter colors

--- src/frontend/src/elements/InfoLetter.tsx
+++ src/frontend/src/elements/InfoLetter.tsx
@@ -2,10 +2,10 @@ import type { PropsWithChildren } from "react";
 
 export default function InfoLetter(props: PropsWithChildren) {
     return (
-        <span className="group relative cursor-default">
+        <span className="group relative cursor-default mx-1">
             ⓘ
-            <div className="bg-black w-5 h-5 group-hover:visible invisible left-1/2 absolute z-40 rotate-45 top-full -translate-x-1/2" />
-            <div className="invisible group-hover:visible absolute top-full mt-2 left-1/2 w-max z-50 max-w-[30vw] -translate-x-1/2 text-sm border border-black bg-white">
+            <div className="bg-accent w-5 h-5 group-hover:visible invisible left-1/2 absolute z-40 rotate-45 top-full -translate-x-1/2" />
+            <div className="invisible group-hover:visible absolute top-full mt-2 left-1/2 w-max z-50 max-w-[25vw] -translate-x-1/2 text-sm border-2 border-accent border-opacity-50 bg-primary py-1 px-2 rounded-md">
                 {props.children}
             </div>
         </span>
