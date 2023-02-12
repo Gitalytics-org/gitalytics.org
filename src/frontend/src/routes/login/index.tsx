5da0f0311bbe0e54ae39fc90da4492aac58c77c9
roriwa <robinrwalter2002@gmail.com>
2023-02-12 12:27:41 +0100

    added link component for app-links

--- src/frontend/src/routes/login/index.tsx
+++ src/frontend/src/routes/login/index.tsx
@@ -1,8 +1,8 @@
 import Footer from "~/components/Footer";
+import AppLink from "~/elements/AppLink";
 import GithubIconSrc from "./icons/github.png";
 import BitbucketIconSrc from "./icons/bitbucket.png";
 import GitlabIconSrc from "./icons/gitlab.png";
-import { Link } from "react-router-dom";
 
 
 export default function LoginPage() {
@@ -11,18 +11,18 @@ export default function LoginPage() {
             <h1 className="text-center text-[10vw] select-none">Login to <span className="text-gitalytics">Gitalytics</span></h1>
             <p className="opacity-50 text-center select-none">select the git provider from which you want to login</p>
             <div className="grow flex justify-around gap-20 px-5">
-                <Link className="my-auto" to="/api/auth/github/login/">
+                <AppLink className="my-auto" href="/api/auth/github/login/">
                     <img className="dark:invert" src={GithubIconSrc} alt="github" />
                     <p className="text-center opacity-50">Login via Github</p>
-                </Link>
-                <Link className="my-auto grayscale cursor-not-allowed"  to="/api/auth/bitbucket/login/" onClick={(e) => e.preventDefault()} title="not available yet">
+                </AppLink>
+                <AppLink className="my-auto grayscale cursor-not-allowed" href="/api/auth/bitbucket/login/" onClick={(e) => e.preventDefault()} title="not available yet">
                     <img src={BitbucketIconSrc} alt="bitbucket" />
                     <p className="text-center opacity-50 line-through">Login via Bitbucket</p>
-                </Link>
-                <Link className="my-auto grayscale cursor-not-allowed"  to="/api/auth/gitlab/login/" onClick={(e) => e.preventDefault()} title="not available yet">
+                </AppLink>
+                <AppLink className="my-auto grayscale cursor-not-allowed" href="/api/auth/gitlab/login/" onClick={(e) => e.preventDefault()} title="not available yet">
                     <img src={GitlabIconSrc} alt="gitlab" />
                     <p className="text-center opacity-50 line-through">Login via Gitlab</p>
-                </Link>
+                </AppLink>
             </div>
         </div>
         <Footer />
