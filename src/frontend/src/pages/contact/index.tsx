df3a7198f4e407a28624a3fa850077b441572a9e
roriwa <robinrwalter2002@gmail.com>
2023-02-12 16:03:42 +0100

    updated contact us page

--- src/frontend/src/pages/contact/index.tsx
+++ src/frontend/src/pages/contact/index.tsx
@@ -7,11 +7,11 @@ export default function ContactPage() {
         <div className="grid grid-cols-2 w-screen h-screen">
             <img className="h-full m-auto p-5" src={GitalyticsLogoSrc} alt="logo" />
             <div className="my-auto min-h-[50vh]">
-                <h1 className="text-center text-[6vw]">Contact Us</h1>
+                <h1 className="text-center text-[6vw] select-none">Contact Us</h1>
                 <div className="grid grid-cols-2">
-                    <span>Email:</span>
-                    <a className="hover:underline text-blue-600" href="mailto:developer@gitalytics.org" target="_blank" rel="noreferrer">developer@gitalytics.org</a>
-                    <span>Github:</span>
+                    {/* <span className="select-none">Email:</span>
+                    <a className="hover:underline text-blue-600" href="mailto:developer@gitalytics.org" target="_blank" rel="noreferrer">developer@gitalytics.org</a> */}
+                    <span className="select-none">Github:</span>
                     <a className="hover:underline text-blue-600" href="https://github.com/konstantinlob/gitalytics.org" target="_blank" rel="noreferrer">konstantinlob/gitalytics.org</a>
                 </div>
             </div>
