8c0fbe584e43225f05638c3400fed39a58844237
= <bennitallarek@gmail.com>
2023-02-20 13:20:58 +0100

    added endpoint for commits

--- src/backend/api/routes/get_commits.route.py
+++ src/backend/api/routes/get_commits.route.py
@@ -3,30 +3,40 @@
 r"""
 
 """
+import json
 import typing
 import fastapi
 import pydantic
-from api.database.models import Repository, Commit
+from api.database.models import Repository, Commit, Workspace
 from api.database.db import createLocalSession
+from sqlalchemy import select
+from datetime import datetime
 
 router = fastapi.APIRouter()
 
-#class CommitViewModel(pydantic.BaseModel):
-#    id: int
-#    name: str
-#
-#    class Config:
-#        orm_mode = True
+class CommitViewModel(pydantic.BaseModel):
+    committed_at: datetime
+    lines_removed: int
+    author_id: int
+    created_at: datetime
+    lines_added: int
+    files_modified: int
+    repository_id: int
+    id: int
 
-#class ResponseModel(pydantic.BaseModel):
-#    commits: list[CommitViewModel]
+    class Config:
+        orm_mode = True
 
-@router.get("/get-commits")
+class ResponseModel(pydantic.BaseModel):
+    commits: list[CommitViewModel]
+
+@router.get("/commits", response_model=ResponseModel)
 async def get_commits():
     r"""
-    list all workspaces in the database
+    list all commits in the workspace
     """
     with createLocalSession() as session:
-        commits = session.query(Commit)
-    
+        statement = select(Commit, Repository).join(Repository.commits).filter(Repository.workspace_id == 1)
+        commits = session.scalars(statement).all()
+
     return {"commits": commits}
