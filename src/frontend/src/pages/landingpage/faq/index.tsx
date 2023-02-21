fa7cd450a72721efed98b90459156c40b343d8ec
roriwa <robinrwalter2002@gmail.com>
2023-02-21 17:35:23 +0100

    fixed some styling

--- src/frontend/src/pages/landingpage/faq/index.tsx
+++ src/frontend/src/pages/landingpage/faq/index.tsx
@@ -4,30 +4,30 @@ import AnswerIconSrc from "./icons/answer.svg";
 
 
 export default function FAQSection() {
-    return <div className="flex flex-col gap-10" id="faq">
-        <div className="flex flex-col gap-2">
-            <h2 className="text-3xl font-bold select-none text-center">
-                Frequently Asked Questions
-            </h2>
+    return <div className="flex flex-col gap-5" id="faq">
+        <h2 className="text-3xl font-bold select-none text-center">
+            Frequently Asked Questions
+        </h2>
+        <SeparatorLine />
+        {FAQList.map((faq, key) => <>
+            <div className="grid grid-cols-[auto,1fr] gap-2 w-full max-w-3xl mx-auto" key={key}>
+                <QuestionIcon />
+                <h3 className="text-2xl font-bold">
+                    {faq.question}
+                </h3>
+                <AnswerIcon />
+                <p className="opacity-80">
+                    {faq.answer}
+                </p>
+            </div>
             <SeparatorLine />
-        </div>
-        {FAQList.map((faq, key) => <div className="grid grid-cols-[auto,1fr] gap-2 w-full max-w-3xl mx-auto" key={key}>
-            <QuestionIcon />
-            <h3 className="text-2xl font-bold">
-                {faq.question}
-            </h3>
-            <AnswerIcon />
-            <p className="opacity-80">
-                {faq.answer}
-            </p>
-            <SeparatorLine />
-        </div>)}
+        </>)}
     </div>;
 }
 
 
 function SeparatorLine() {
-    return <div className="h-1 w-32 from-blue-400 via-blue-700 to-blue-400 bg-gradient-to-r mx-auto col-span-2 rounded-full" />;
+    return <div className="h-1 max-w-xs w-1/5 from-blue-400 via-blue-700 to-blue-400 bg-gradient-to-r mx-auto rounded-full" />;
 }
 
 
