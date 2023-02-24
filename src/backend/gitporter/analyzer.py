44e0fa1e2dd2d4b83aa56d2b08083286fe70a24f
Julius Jänchen <julius.jaenchen@check24.de>
2023-02-24 12:52:28 +0100

    renamed folder

--- src/backend/gitporter/analyzer.py
+++ src/backend/gitporter/analyzer.py
@@ -18,7 +18,7 @@ import git
 from datetime import datetime
 from database import createLocalSession, DatabaseSession, models as dbm
 from database.enums import GitPlatform
-from .gitprovider_stuff import getRepositoryList as get_remote_repositories, RepositoryInfo as RemoteRepositoryInformation
+from .gitprovider import getRepositoryList as get_remote_repositories, RepositoryInfo as RemoteRepositoryInformation
 from .output_analyzer import parseLog
 
 
