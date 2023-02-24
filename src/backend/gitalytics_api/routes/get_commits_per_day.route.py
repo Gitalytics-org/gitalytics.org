1d26079e661adaee0856da87dc3bbb723d85e03c
roriwa <robinrwalter2002@gmail.com>
2023-02-24 20:45:40 +0100

    moved and code-reformat

--- src/backend/gitalytics_api/routes/get_commits_per_day.route.py
+++ src/backend/gitalytics_api/routes/get_commits_per_day.route.py
@@ -5,12 +5,13 @@ r"""
 """
 import fastapi
 import pydantic
-from api.database.models import Repository, Commit
-from api.database.db import createLocalSession
-from sqlalchemy import select, func
-from sqlalchemy.sql import extract
+from database import models as dbm
+from database.db import createLocalSession
+import sqlalchemy as sql
+
 
 router = fastapi.APIRouter()
+
 
 class ResponseModel(pydantic.BaseModel):
     commits_per_day: dict[str, int]
@@ -22,13 +23,11 @@ async def get_commits_per_day(workspace_id: int, year: int):
     get commits per day in the workspace from a specific year
     """
     with createLocalSession() as connection:
-
-        commits_per_day = connection.query(func.date(Commit.committed_at), func.count())\
-            .group_by(func.date(Commit.committed_at))\
-            .join(Repository.commits)\
-            .filter(Repository.workspace_id == workspace_id)\
-            .filter(extract('year', Commit.committed_at) == year)\
+        commits_per_day = connection.query(sql.func.date(dbm.Commit.committed_at), sql.func.count()) \
+            .group_by(sql.func.date(dbm.Commit.committed_at)) \
+            .join(dbm.Repository.commits) \
+            .filter(dbm.Repository.workspace_id == workspace_id) \
+            .filter(sql.func.extract('year', dbm.Commit.committed_at) == year) \
             .all()
-
 
     return {"commits_per_day": commits_per_day}
