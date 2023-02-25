882dd94ae0019d40d98ef406d00686297c7fe615
Julius Jänchen <julius.jaenchen@check24.de>
2023-02-25 13:17:33 +0100

    moved definition above usage

--- src/backend/gitporter/gitprovider/github.py
+++ src/backend/gitporter/gitprovider/github.py
@@ -9,17 +9,16 @@ import httpx
 from .common import RemoteRepositoryInformation
 
 
-def get_remote_repositories(workspace: str) -> t.List[RemoteRepositoryInformation]:
-    response = httpx.get(f"https://api.github.com/users/{workspace}/repos")
-    response.raise_for_status()
-    github_repositories: t.List[GithubRepoListResponse] = response.json()
-
-    return [RemoteRepositoryInformation(repository_name=repo['name'], clone_url=repo['clone_url']) for repo in github_repositories]
-
-
 class GithubRepoListResponse(t.TypedDict):
     name: str
     full_name: str
     git_url: str
     ssh_url: str
     clone_url: str
+
+def get_remote_repositories(workspace: str) -> t.List[RemoteRepositoryInformation]:
+    response = httpx.get(f"https://api.github.com/users/{workspace}/repos")
+    response.raise_for_status()
+    github_repositories: t.List[GithubRepoListResponse] = response.json()
+
+    return [RemoteRepositoryInformation(repository_name=repo['name'], clone_url=repo['clone_url']) for repo in github_repositories]
