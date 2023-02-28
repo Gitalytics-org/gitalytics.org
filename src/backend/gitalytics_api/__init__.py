f083dbfb3fb1dce10ba311402d0ac2fbe16c5a91
roriwa <103991412+roriwa@users.noreply.github.com>
2023-02-28 21:38:06 +0100

    changed timing function

--- src/backend/gitalytics_api/__init__.py
+++ src/backend/gitalytics_api/__init__.py
@@ -33,9 +33,9 @@ app.add_middleware(
 if __debug__:
     @app.middleware("http")
     async def add_process_time_header(request: fastapi.Request, call_next):
-        start_time = time.perf_counter_ns()
+        start_time = time.perf_counter()
         response = await call_next(request)
-        process_time = time.perf_counter_ns() - start_time
+        process_time = time.perf_counter() - start_time
         response.headers["X-Process-Time"] = str(process_time)
         return response
 
