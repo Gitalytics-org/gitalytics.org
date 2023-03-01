4f38c71983759c8064d194c1a516e90f5e004b8f
Benni0706 <bennitallarek@gmail.com>
2023-03-01 12:17:13 +0100

    added verification to commits_per_day endpoint. works with repository_access

--- src/backend/gitalytics_api/routes/get_commits_per_day.py
+++ src/backend/gitalytics_api/routes/get_commits_per_day.py
@@ -8,6 +8,7 @@ import pydantic
 from database import models as dbm
 from database.db import createLocalSession
 import sqlalchemy as sql
+from gitalytics_api.common import SessionToken
 
 
 router = fastapi.APIRouter()
@@ -18,13 +19,14 @@ class ResponseModel(pydantic.BaseModel):
 
 
 @router.get("/commits_per_day/", response_model=ResponseModel)
-async def get_commits_per_day(workspace_id: int, year: int):
+async def get_commits_per_day(workspace_id: int, year: int, session: dbm.Session = SessionToken):
     r"""
     get commits per day in the workspace from a specific year
     """
     with createLocalSession() as connection:
         commits_per_day = connection.query(sql.func.date(dbm.Commit.committed_at), sql.func.count()) \
             .group_by(sql.func.date(dbm.Commit.committed_at)) \
+            .join(dbm.Session.repositories) \
             .join(dbm.Repository.commits) \
             .filter(dbm.Repository.workspace_id == workspace_id) \
             .filter(sql.func.extract('year', dbm.Commit.committed_at) == year) \
