cb91398af8c38cd34ccc2123921c9a7407a18dd2
Julius Jänchen <julius.jaenchen@check24.de>
2023-03-01 08:33:23 +0100

    refactored db session to connection

--- src/backend/gitporter/update_workspace.py
+++ src/backend/gitporter/update_workspace.py
@@ -1,26 +1,26 @@
-from database import createLocalSession, DatabaseSession, models as dbm
+from database import createLocalSession, DatabaseSession as DatabaseConnection, models as dbm
 from database.enums import GitPlatform
 from .gitprovider import get_remote_repositories
 from .repository_actions import update_repository, initialize_repository
 
 def update_workspace(workspace_name: str, platform: GitPlatform):
-    with createLocalSession() as session:
-        workspace = session.query(dbm.Workspace) \
+    with createLocalSession() as connection:
+        workspace = connection.query(dbm.Workspace) \
             .filter(dbm.Workspace.name == workspace_name,
                     dbm.Workspace.platform == platform) \
             .one_or_none()
         if workspace is None:
             workspace = dbm.Workspace(name=workspace_name, platform=platform)
-            session.add(workspace)
-        update_repositories_in_workspace(workspace=workspace, platform=platform, session=session)
+            connection.add(workspace)
+        update_repositories_in_workspace(workspace=workspace, platform=platform, connection=connection)
 
-def update_repositories_in_workspace(workspace: dbm.Workspace, platform: GitPlatform, session: DatabaseSession):
+def update_repositories_in_workspace(workspace: dbm.Workspace, platform: GitPlatform, connection: DatabaseConnection):
     for remote_repository in get_remote_repositories(platform=platform, workspace=workspace.name):
-        exists_in_databse = bool(session.query(dbm.Repository) \
+        exists_in_databse = bool(connection.query(dbm.Repository) \
                         .filter(dbm.Repository.name == remote_repository.repository_name,
                                 dbm.Repository.workspace == workspace) \
                         .one_or_none())
         if exists_in_databse:
-            update_repository(workspace=workspace, remote_repository=remote_repository, session=session)
+            update_repository(workspace=workspace, remote_repository=remote_repository, session=connection)
         else:
-            initialize_repository(workspace=workspace, remote_repository=remote_repository, session=session)
+            initialize_repository(workspace=workspace, remote_repository=remote_repository, session=connection)
