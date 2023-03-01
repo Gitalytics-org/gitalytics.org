0629194126751447d623fcce087b4928271d9807
Julius Jänchen <julius.jaenchen@check24.de>
2023-03-01 12:38:04 +0100

    formating

--- src/backend/gitporter/update_all_workspaces.py
+++ src/backend/gitporter/update_all_workspaces.py
@@ -1,6 +1,7 @@
 from database import createLocalSession, models as dbm
 from .update_session_repositories import update_session_repositories
 
+
 def update_all_workspaces():
     r"""
     TODO: ignore workspaces with sessions older than 30 days
