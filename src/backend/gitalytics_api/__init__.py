09e2938286a738c742f6fb7f9f65a17a3ec377dd
Julius Jänchen <julius.jaenchen@check24.de>
2023-03-02 17:25:21 +0100

    shuffled imports

--- src/backend/gitalytics_api/__init__.py
+++ src/backend/gitalytics_api/__init__.py
@@ -1,4 +1,4 @@
 from .env_variables import env
-from .uvicorn_server import app
 from .cookies import session_from_cookies, EncryptedCookieStorage
 from .httpx_extention import HttpxBearerAuth
+from .uvicorn_server import app
