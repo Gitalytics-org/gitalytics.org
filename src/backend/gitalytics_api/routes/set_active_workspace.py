c59c99bf531081ad1fc61fc8a30cfcd6c357056d
Julius Jänchen <julius.jaenchen@check24.de>
2023-03-02 10:35:56 +0100

    added basic set-active-workspace endpoint

---
+++ src/backend/gitalytics_api/routes/set_active_workspace.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+import typing as t
+import fastapi
+import pydantic
+import database.models as dbm
+from database.db import createLocalSession
+from gitalytics_api.cookies import SessionToken, EncryptedCookieStorage
+from gitalytics_api.enums import CookieKey
+
+
+router = fastapi.APIRouter()
+
+
+class ResponseModel(pydantic.BaseModel):
+    status: str = "success"
+    message: str | None = None
+
+
+
+@router.put("/set-active-workspace", response_model=ResponseModel)
+async def get_workspaces(workspace_name: str, cookie_storage: EncryptedCookieStorage = EncryptedCookieStorage):
+    r"""
+    list all workspaces for the current user (session)
+    """
+    
+    cookie_storage.set(key=CookieKey.ACTIVE_WORKSPACE_NAME, value=workspace_name)
+    return
