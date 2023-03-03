0550bcb26190ef5c4cd97fe2fcfd0b4c7d89e17c
Julius Jänchen <julius.jaenchen@check24.de>
2023-03-03 10:27:04 +0100

    filtered out older sessions from update-all

--- src/backend/gitporter/update_all_workspaces.py
+++ src/backend/gitporter/update_all_workspaces.py
@@ -1,5 +1,6 @@
 from database import createLocalSession, models as dbm
 from .update_session_repositories import update_session_repositories
+from datetime import datetime, timedelta
 
 
 def update_all_workspaces():
@@ -7,5 +8,8 @@ def update_all_workspaces():
     TODO: ignore workspaces with sessions older than 30 days
     """
     with createLocalSession() as connection:
-        for session in connection.query(dbm.Session).all():
+        recent_sessions = connection.query(dbm.Session) \
+            .filter(dbm.Session.last_seen > (datetime.now() - timedelta(days=30))) \
+            .all()
+        for session in recent_sessions:
             update_session_repositories(session_id=session.id)
