471f9b1195c1cfad6326e7119fe288d86288c587
roriwa <robinrwalter2002@gmail.com>
2023-03-06 18:43:38 +0100

    (hopefully) fixed/improved commits-per-x endpoints

--- src/backend/gitalytics_api/routes/get_commits.py
+++ src/backend/gitalytics_api/routes/get_commits.py
@@ -22,33 +22,21 @@ def get_database_connection():
         db.close()
 
 
-@fastapi.Depends
-def get_commit_count_subquery(
-        connection: sql.orm.Session = get_database_connection,
-        workspace_id: int = active_workspace_id,
-        year: int = fastapi.Query()
-):
-    repos = connection.query(dbm.Repository.id).filter(dbm.Repository.workspace_id == workspace_id)
-    commits = connection.query(dbm.Commit.id).filter(dbm.Commit.repository_id.in_(repos))
-    return connection \
-        .query(sql.func.count().label("count"),
-               dbm.Commit.committed_at.label("date")) \
-        .filter(dbm.Commit.id.in_(commits),
-                sql.func.extract("year", dbm.Commit.committed_at) == year) \
-        .group_by(sql.func.extract("month", dbm.Commit.committed_at),
-                  sql.func.extract("day", dbm.Commit.committed_at)) \
-        .subquery()
-
-
 @router.get("/commits-per-hour")
 async def get_commits_per_hour(
         connection: sql.orm.Session = get_database_connection,
-        subquery=get_commit_count_subquery
+        workspace_id: int = active_workspace_id,
+        year: int = fastapi.Query(gt=0),
 ):
+    repos = connection\
+        .query(dbm.Repository.id)\
+        .filter(dbm.Repository.workspace_id == workspace_id)
     stats = connection \
-        .query(sql.func.sum(subquery.c.count).label("count"),
-               sql.func.extract("hour", subquery.c.date).label("hour")) \
-        .group_by(sql.func.extract("hour", subquery.c.date)) \
+        .query(sql.func.count().label("count"),
+               sql.func.extract("hour", dbm.Commit.committed_at).label("hour")) \
+        .filter(dbm.Commit.repository_id.in_(repos),
+                sql.func.extract("year", dbm.Commit.committed_at) == year) \
+        .group_by(sql.func.extract("hour", dbm.Commit.committed_at)) \
         .all()
 
     return {row.hour: row.count for row in stats}
@@ -57,17 +45,23 @@ async def get_commits_per_hour(
 @router.get("/commits-per-weekday")
 async def get_commits_per_weekday(
         connection: sql.orm.Session = get_database_connection,
-        subquery=get_commit_count_subquery
+        workspace_id: int = active_workspace_id,
+        year: int = fastapi.Query(gt=0),
 ):
     r"""
     please note:
         weekday of 0 means sunday
     """
     # 'dow' == day-of-week
+    repos = connection\
+        .query(dbm.Repository.id)\
+        .filter(dbm.Repository.workspace_id == workspace_id)
     stats = connection \
-        .query(sql.func.sum(subquery.c.count).label("count"),
-               sql.func.extract("dow", subquery.c.date).label("weekday")) \
-        .group_by(sql.func.extract("dow", subquery.c.date)) \
+        .query(sql.func.count().label("count"),
+               sql.func.extract("dow", dbm.Commit.committed_at).label("weekday")) \
+        .filter(dbm.Commit.repository_id.in_(repos),
+                sql.func.extract("year", dbm.Commit.committed_at) == year) \
+        .group_by(sql.func.extract("dow", dbm.Commit.committed_at)) \
         .all()
 
     # could add to convert 0=sunday to 0=monday (wd - 1) % 7
@@ -78,12 +72,18 @@ async def get_commits_per_weekday(
 @router.get("/commits-per-day-in-month")
 async def get_commits_per_day_in_month(
         connection: sql.orm.Session = get_database_connection,
-        subquery=get_commit_count_subquery
+        workspace_id: int = active_workspace_id,
+        year: int = fastapi.Query(gt=0),
 ):
+    repos = connection\
+        .query(dbm.Repository.id)\
+        .filter(dbm.Repository.workspace_id == workspace_id)
     stats = connection \
-        .query(sql.func.sum(subquery.c.count).label("count"),
-               sql.func.extract("day", subquery.c.date).label("day")) \
-        .group_by(sql.func.extract("day", subquery.c.date)) \
+        .query(sql.func.count().label("count"),
+               sql.func.extract("day", dbm.Commit.committed_at).label("day")) \
+        .filter(dbm.Commit.repository_id.in_(repos),
+                sql.func.extract("year", dbm.Commit.committed_at) == year) \
+        .group_by(sql.func.extract("day", dbm.Commit.committed_at)) \
         .all()
 
     return {row.day: row.count for row in stats}
@@ -92,12 +92,18 @@ async def get_commits_per_day_in_month(
 @router.get("/commits-per-week")
 async def get_commits_per_week(
         connection: sql.orm.Session = get_database_connection,
-        subquery=get_commit_count_subquery
+        workspace_id: int = active_workspace_id,
+        year: int = fastapi.Query(gt=0),
 ):
+    repos = connection\
+        .query(dbm.Repository.id)\
+        .filter(dbm.Repository.workspace_id == workspace_id)
     stats = connection \
-        .query(sql.func.sum(subquery.c.count).label("count"),
-               sql.func.extract("week", subquery.c.date).label("week")) \
-        .group_by(sql.func.extract("week", subquery.c.date)) \
+        .query(sql.func.count().label("count"),
+               sql.func.extract("week", dbm.Commit.committed_at).label("week")) \
+        .filter(dbm.Commit.repository_id.in_(repos),
+                sql.func.extract("year", dbm.Commit.committed_at) == year) \
+        .group_by(sql.func.extract("week", dbm.Commit.committed_at)) \
         .all()
 
     return {row.week: row.count for row in stats}
@@ -106,12 +112,18 @@ async def get_commits_per_week(
 @router.get("/commits-per-month")
 async def get_commits_per_month(
         connection: sql.orm.Session = get_database_connection,
-        subquery=get_commit_count_subquery
+        workspace_id: int = active_workspace_id,
+        year: int = fastapi.Query(gt=0),
 ):
+    repos = connection\
+        .query(dbm.Repository.id)\
+        .filter(dbm.Repository.workspace_id == workspace_id)
     stats = connection \
-        .query(sql.func.sum(subquery.c.count).label("count"),
-               sql.func.extract("month", subquery.c.date).label("month")) \
-        .group_by(sql.func.extract("month", subquery.c.date)) \
+        .query(sql.func.count().label("count"),
+               sql.func.extract("month", dbm.Commit.committed_at).label("month")) \
+        .filter(dbm.Commit.repository_id.in_(repos),
+                sql.func.extract("year", dbm.Commit.committed_at) == year) \
+        .group_by(sql.func.extract("month", dbm.Commit.committed_at)) \
         .all()
 
     return {row.month: row.count for row in stats}
