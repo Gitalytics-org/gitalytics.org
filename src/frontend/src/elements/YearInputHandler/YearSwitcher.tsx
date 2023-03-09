b8483ac5d9eef7f67c58d5c503ff915ae5984362
Julius Jänchen <julius.jaenchen@check24.de>
2023-03-09 15:29:16 +0100

    created icons folder

--- src/frontend/src/elements/YearInputHandler/YearSwitcher.tsx
+++ src/frontend/src/elements/YearInputHandler/YearSwitcher.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import ChevronRight from "../ChevronRight";
+import ChevronRight from "~/icons/ChevronRight";
 
 
 function getCurrentYear() {
