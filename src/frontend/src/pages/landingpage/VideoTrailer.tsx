56961e3939e85ee020de942661f4b521a319145e
roriwa <robinrwalter2002@gmail.com>
2023-03-11 12:42:29 +0100

    added video-trailer section

---
+++ src/frontend/src/pages/landingpage/VideoTrailer.tsx
@@ -0,0 +1,13 @@
+export default function VideoTrailer() {
+    return <div>
+        <iframe
+            className="mx-auto"
+            width="560"
+            height="315"
+            src="https://www.youtube-nocookie.com/embed/-fxwoXAcLYA"
+            title="Gitalytics.org"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+        ></iframe>
+    </div>;
+}
