da500bf3e2a7b9993dbd994f0adbf75872b4cd86
Julius Jänchen <julius.jaenchen@check24.de>
2023-03-11 12:08:36 +0100

    formatting

--- src/backend/gitalytics_api/routes/set_active_workspace.py
+++ src/backend/gitalytics_api/routes/set_active_workspace.py
@@ -7,8 +7,8 @@ import typing as t
 import fastapi
 import pydantic
 import database.models as dbm
-from database.db import createLocalSession
-from gitalytics_api.cookies import session_from_cookies, EncryptedCookieStorage
+from database.db import DatabaseSession
+from gitalytics_api import session_from_cookies, EncryptedCookieStorage, get_database_connection
 from gitalytics_api.enums import CookieKey
 
 
@@ -20,19 +20,23 @@ class ResponseModel(pydantic.BaseModel):
 
 
 @router.put("/set-active-workspace", response_model=ResponseModel)
-async def set_active_workspace(workspace_name: str, cookie_storage: EncryptedCookieStorage = EncryptedCookieStorage,
-                               session: dbm.Session = session_from_cookies):
+async def set_active_workspace(
+    workspace_name: str, 
+    cookie_storage: EncryptedCookieStorage = EncryptedCookieStorage,
+    session: dbm.Session = session_from_cookies,
+    db_connection: DatabaseSession = get_database_connection,
+):
     r"""
     sets the active workspace for a user's session
     """
-    with createLocalSession() as connection:
-        workspace: dbm.Workspace = connection.query(dbm.Workspace) \
-            .select_from(dbm.Session) \
-            .join(dbm.Repository, dbm.Session.repositories) \
-            .join(dbm.Workspace) \
-            .filter(dbm.Session.id == session.id) \
-            .filter(dbm.Workspace.name == workspace_name) \
-            .one_or_none()
+    workspace: dbm.Workspace = db_connection \
+        .query(dbm.Workspace) \
+        .select_from(dbm.Session) \
+        .join(dbm.Repository, dbm.Session.repositories) \
+        .join(dbm.Workspace) \
+        .filter(dbm.Session.id == session.id) \
+        .filter(dbm.Workspace.name == workspace_name) \
+        .one_or_none()
 
     if workspace is None:
         raise fastapi.HTTPException(fastapi.status.HTTP_406_NOT_ACCEPTABLE)
