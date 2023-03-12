01170d9578d274b6ae1ca5600953ac3c65d731d0
roriwa <robinrwalter2002@gmail.com>
2023-03-12 12:17:26 +0100

    extended the what is this section

--- src/frontend/src/pages/landingpage/WhatIsThis/index.tsx
+++ src/frontend/src/pages/landingpage/WhatIsThis/index.tsx
@@ -1,26 +1,58 @@
 import FadeInBox from "~/elements/animations/FadeInBox";
-import GitGraph from "./images/git-graph.png";
+import { type PropsWithChildren } from "react";
+import GitHubIconSrc from "@assets/github.png";
+import BitbucketIconSrc from "@assets/bitbucket.png";
+import GitLabIconSrc from "@assets/gitlab.png";
+import CodeBoxSrc from "./images/code-box.svg";
+import GitGraph from "./images/activity-graph.png";
 import ChartGraph from "./images/line-graph.png";
 
 
 export default function WhatIsThis() {
-    return <div className="grid grid-cols-2 gap-5 p-2">
+    return <div className="grid grid-cols-2 gap-10 p-2">
         <FadeInBox from="left">
-            <img src={GitGraph} alt="git-graph" />
+            <ProviderGroupIcon />
         </FadeInBox>
-        <div className="grid">
-            <p className="m-auto text-center">
-                See how many commits you had last year and all the years before
-            </p>
-        </div>
+        <TextSection>
+            We offer analysis for GitHub, Bitbucket and GitLab
+        </TextSection>
 
-        <div className="grid">
-            <p className="m-auto text-center">
-                View some other stuff
-            </p>
-        </div>
+        <TextSection>
+            Instead of analyzing only one repository, we can analyze an entire workspace. A feature, that many other products lack
+        </TextSection>
         <FadeInBox from="right">
-            <img className="bg-blend-color-burn" src={ChartGraph} alt="chart-graph" />
+            <img className="max-h-64 mx-auto" src={CodeBoxSrc} alt="Code-Box" />
         </FadeInBox>
+
+        <FadeInBox from="left">
+            <img className="mx-auto" src={GitGraph} alt="git-graph" />
+        </FadeInBox>
+        <TextSection>
+            For example you can see how many commits you had last year or all the years before.<br />
+            On top of that you can also compare your stats from different years.
+        </TextSection>
+
+        <TextSection>
+            Currently wee offer many different graphs so your can gain the most insight
+        </TextSection>
+        <FadeInBox from="right">
+            <img className="mx-auto" src={ChartGraph} alt="chart-graph" />
+        </FadeInBox>
+    </div>;
+}
+
+
+function TextSection(props: PropsWithChildren) {
+    return <div className="grid place-content-center text-center">
+        {props.children}
+    </div>;
+}
+
+
+function ProviderGroupIcon() {
+    return <div className="flex gap-2 justify-evenly">
+        <img className="w-1/4 dark:invert" src={GitHubIconSrc} alt="GitHub" />
+        <img className="w-1/4" src={BitbucketIconSrc} alt="Bitbucket" />
+        <img className="w-1/4" src={GitLabIconSrc} alt="GitLab" />
     </div>;
 }
